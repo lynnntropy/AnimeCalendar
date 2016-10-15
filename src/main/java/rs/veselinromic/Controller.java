43fb8dabcfd2c10cfeb4cc7cb2345a6ac2f329eb
Veselin <omegavesko@gmail.com>
2016-10-15 23:01:21 +0200

    Added providers for Crunchyroll and Funimation.
    Funi is currently non-functional as the HTTP request hits their bot protection screen.

--- src/main/java/rs/veselinromic/Controller.java
+++ src/main/java/rs/veselinromic/Controller.java
@@ -3,7 +3,8 @@ package rs.veselinromic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rs.veselinromic.model.ScheduleItem;
-import rs.veselinromic.scrape.Crunchyroll;
+import rs.veselinromic.provider.Crunchyroll;
+import rs.veselinromic.provider.Funimation;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,7 +15,13 @@ public class Controller
     @RequestMapping("/schedule/crunchyroll")
     public List<ScheduleItem> getCrunchyroll() throws IOException
     {
-        return new Crunchyroll().scrape();
+        return new Crunchyroll().retrieveSchedule();
+    }
+
+    @RequestMapping("/schedule/funimation")
+    public List<ScheduleItem> getFuni() throws IOException
+    {
+        return new Funimation().retrieveSchedule();
     }
 
     @RequestMapping("/hello")
